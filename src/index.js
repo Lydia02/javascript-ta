2a5636912fe8be3b87612ce0f9c7c6d2fc237f7d
Lydia Ojoawo <ojoawolydia@gmail.com>
2022-06-09 12:43:16 +0100

    Technical Assesssment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 // complete the function
 function prime (num) {
   // code goes here
+  if (num === 1){
+    return false
+  } else if(num === 2){
+    return true
+  } else{
+    for (let i = 2; i < num; i++){
+      if(num %  i === 0){
+        return false
+      }
+    }
+  } return true
+  
 }
 
 function solution (arg) {
